cc83ff6ce4dc178f9cb1495d2afb594a7ac0dac0
Xezioh <Xezioh@pm.me>
2023-03-20 15:24:01 +0100

    still doing daisy

--- keyboards/ktec/daisy/keymaps/vial/rules.mk
+++ keyboards/ktec/daisy/keymaps/vial/rules.mk
@@ -1,21 +1,17 @@
-# MCU name
-MCU = atmega32u4
+MCU = atmega32u4            # MCU name
+BOOTLOADER = atmel-dfu      # Bootloader selection
 
-# Bootloader selection
-BOOTLOADER = atmel-dfu
 
-# Build Options
-#   change yes to no to disable
-#
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
+NKRO_ENABLE = yes            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = yes      # Enable WS2812 RGB underlight?
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+
+
+QMK_SETTINGS        = no
+COMBO_ENABLE        = yes
+LTO_ENABLE          = yes
 
 
 VIAL_INSECURE = yes
